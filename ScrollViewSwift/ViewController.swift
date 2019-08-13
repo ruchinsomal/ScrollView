7dd3b7cf496271bfcc6f4a4eff8abdf85647018b
Ruchin Somal <ruchin.somal@SEW-APAC-903.local>
2019-08-13 12:03:03 +0530

    converted to Swift 5

--- ScrollViewSwift/ViewController.swift
+++ ScrollViewSwift/ViewController.swift
@@ -25,24 +25,24 @@ class ViewController: UIViewController,UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate=self;
-        self.scrollView.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
+        self.scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
         let scrollViewWidth:CGFloat = self.scrollView.frame.width
         let scrollViewHeight:CGFloat = self.scrollView.frame.height
-        imgOne = UIImageView(frame: CGRectMake(0, 0,scrollViewWidth, scrollViewHeight))
+        imgOne = UIImageView(frame: CGRect(x: 0, y: 0,width: scrollViewWidth, height: scrollViewHeight))
         imgOne.image = UIImage(named: "mobile-1")
         imgOne.backgroundColor=UIColor(red: 231/255.0, green: 151/255.0, blue: 56/255.0, alpha: 1.0)
-        imgTwo = UIImageView(frame: CGRectMake(scrollViewWidth, 0,scrollViewWidth, scrollViewHeight))
+        imgTwo = UIImageView(frame: CGRect(x: scrollViewWidth, y: 0,width: scrollViewWidth, height: scrollViewHeight))
         imgTwo.backgroundColor=UIColor(red: 241/255.0, green: 92/255.0, blue: 86/255.0, alpha: 1.0)
         imgTwo.image = UIImage(named: "mobile_2")
-        imgThree = UIImageView(frame: CGRectMake(scrollViewWidth*2, 0,scrollViewWidth, scrollViewHeight))
+        imgThree = UIImageView(frame: CGRect(x: scrollViewWidth*2, y: 0,width: scrollViewWidth, height: scrollViewHeight))
         imgThree.backgroundColor=UIColor(red: 173/255.0, green: 97/255.0, blue: 169/255.0, alpha: 1.0)
         imgThree.image = UIImage(named: "mobile_3")
         
-        imgFour = UIImageView(frame: CGRectMake(-scrollViewWidth*2, 0,scrollViewWidth, scrollViewHeight))
+        imgFour = UIImageView(frame: CGRect(x: -scrollViewWidth*2, y: 0,width: scrollViewWidth, height: scrollViewHeight))
         imgFour.image = UIImage(named: "bg-3")
-        imgFive = UIImageView(frame: CGRectMake(-scrollViewWidth, 0,scrollViewWidth, scrollViewHeight))
+        imgFive = UIImageView(frame: CGRect(x: -scrollViewWidth, y: 0,width: scrollViewWidth, height: scrollViewHeight))
         imgFive.image = UIImage(named: "bg-2")
-        imgSix = UIImageView(frame: CGRectMake(0, 0,scrollViewWidth, scrollViewHeight))
+        imgSix = UIImageView(frame: CGRect(x: 0, y: 0,width: scrollViewWidth, height: scrollViewHeight))
         imgSix.image = UIImage(named: "bg-1")
         self.scrollView.addSubview(imgOne)
         self.scrollView.addSubview(imgTwo)
@@ -50,7 +50,7 @@ class ViewController: UIViewController,UIScrollViewDelegate {
         self.scrollView.addSubview(imgFour)
         self.scrollView.addSubview(imgFive)
         self.scrollView.addSubview(imgSix)
-        self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.width * 3,self.scrollView.frame.height)
+        self.scrollView.contentSize = CGSize(width: self.scrollView.frame.width * 3,height: self.scrollView.frame.height)
         self.pageControl.currentPage = 0
         moveImage = imgSix.center.x
         moveImage1 = imgFive.center.x
@@ -63,16 +63,16 @@ class ViewController: UIViewController,UIScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView){
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView){
         // Test the offset and calculate the current page after scrolling ends
-        let pageWidth:CGFloat = CGRectGetWidth(scrollView.frame)
+        let pageWidth:CGFloat = scrollView.frame.width
         let currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
         // Change the indicator
         self.pageControl.currentPage = Int(currentPage)
         //print("count:\(Int(currentPage))")
     }
     
-    func scrollViewDidScroll(scrollView: UIScrollView) {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let scrollOffset:CGFloat = scrollView.contentOffset.x * 2
         imgSix.center.x = scrollOffset + moveImage
         imgFive.center.x = scrollOffset + moveImage1
